9755e1947417190d4b8f9b1eed1d03b9ebf71528
Jeff Petrous <ecirrusconsulting@gmail.com>
2020-02-09 18:49:38 -0500

    commiting stack

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "elb_address" {
   value = aws_elb.web.dns_name 
 }
 output "addresses" { 
-  value = aws_instance.web.[*].public_ip 
+  value = aws_instance.web[*].public_ip 
 } 
 output "public_subnet_id" { 
   value = module.vpc_basic.public_subnet_id 
